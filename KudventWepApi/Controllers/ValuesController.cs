22261908c788062cfcf53d096b94e8de3f2c1bac
Praveen Raveendran <Praveen.Raveendran@hotmail.com>
2018-07-05 22:00:23 +0530

    1-Removed Authorize attribute for testing the values controller

--- KudventWepApi/Controllers/ValuesController.cs
+++ KudventWepApi/Controllers/ValuesController.cs
@@ -7,7 +7,7 @@ using System.Web.Http;
 
 namespace KudventWepApi.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class ValuesController : ApiController
     {
         // GET api/values
